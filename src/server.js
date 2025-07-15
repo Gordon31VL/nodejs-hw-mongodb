0c422c43e508d6abe689eef6b0081e1d3b760550
GordonVL <ladaniv123@gmail.com>
2025-07-15 18:30:38 +0200

    complete task

---
+++ src/server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactsById } from './services/contacts.js';
+
+
+export const setupServer = () => {
+    const app = express();
+    const PORT = Number(getEnvVar('PORT', '3000')); 
+        
+    app.use(express.json());
+    app.use(cors());
+    app.use(pino({
+        transport: {
+            target: 'pino-pretty'
+        }
+    }));
+
+    app.get('/contacts', async(req, res) => {
+        const contacts = await getAllContacts();
+        res.status(200).json({
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res) => {
+        const { contactId } = req.params;
+        const contact = await getContactsById(contactId);
+        
+        if (!contact) {
+	        res.status(404).json({
+                message: 'Contact not found'
+            });
+            
+	        return;
+	    }
+
+        res.status(200).json({
+            data: contact,
+        });
+    });
+
+    app.use((req, res, next) => {
+        res.status(404).json({
+            message: 'Route not found',
+        });
+    });
+    
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+    
+    return app;
+};
