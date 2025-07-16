c1a97b0daa1f44c28bd44b5df4f436ffe3d6ba8d
GordonVL <ladaniv123@gmail.com>
2025-07-16 19:19:33 +0200

    fix response

--- src/server.js
+++ src/server.js
@@ -20,6 +20,8 @@ export const setupServer = () => {
     app.get('/contacts', async(req, res) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
             data: contacts,
         });
     });
@@ -37,6 +39,8 @@ export const setupServer = () => {
 	    }
 
         res.status(200).json({
+            status: 200,
+	        message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         });
     });
