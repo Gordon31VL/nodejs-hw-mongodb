2e39ac3e05cec662fee3a3abd9b39004b2f874b0
GordonVL <ladaniv123@gmail.com>
2025-08-08 17:50:18 +0200

    fix patch in task 5

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -2,14 +2,14 @@ import createHttpError from "http-errors";
 
 export const validateBody = (schema) => async (req, res, next) => {
     try {
-        await schema.validateAsync(req.body, {
-            abortEarly: false,
-        });
+        console.log('Body для валідації:', req.body); // Додайте це
+        const { error } = schema.validate(req.body);
+        if (error) {
+            console.log('Помилка валідації:', error.details); // Додайте це
+            throw createHttpError(400, `Validation error: ${error.details.map(detail => detail.message).join(', ')}`);
+        }
         next();
     } catch (err) {
-        const error = createHttpError(400, 'Bad Request', {
-            errors: err.details,
-        });
-        next(error);
+        next(err);
     }
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -45,7 +45,7 @@ export const deleteContact = async (contactId, userId) => {
     return contact;
 };
 
-export const updateContact = async (contactId, userId, payload, option = {}) => {
+export const updateContact = async (contactId, payload, userId, option = {}) => {
     const rawResult = await ContactsCollection.findOneAndUpdate(
         { _id: contactId, userId },
         payload,
