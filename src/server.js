7ad090f2079da8305a5ebb16f8a846eec4b16f8d
GordonVL <ladaniv123@gmail.com>
2025-08-13 20:38:06 +0200

    complete task

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 export const setupServer = () => {
@@ -20,6 +21,7 @@ export const setupServer = () => {
             target: 'pino-pretty'
         }
     }));
+    app.use('/api-docs', swaggerDocs());
 
     app.use(router);
     
